9f95c283b83aff9abe9880ece4f8770dfe26c307
Guy Menahem <guy@thegoodguy.io>
2023-05-10 22:03:54 +0300

    add Kuberenets Plugin

--- guy-backstage/packages/app/src/components/catalog/EntityPage.tsx
+++ guy-backstage/packages/app/src/components/catalog/EntityPage.tsx
@@ -53,6 +53,7 @@ import {
   RELATION_PROVIDES_API,
 } from '@backstage/catalog-model';
 
+import { EntityKubernetesContent } from '@backstage/plugin-kubernetes';
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 
@@ -160,6 +161,10 @@ const serviceEntityPage = (
           <EntityDependsOnResourcesCard variant="gridItem" />
         </Grid>
       </Grid>
+    </EntityLayout.Route>
+
+    <EntityLayout.Route path="/kubernetes" title="Kubernetes">
+      <EntityKubernetesContent refreshIntervalMs={30000} />
     </EntityLayout.Route>
 
     <EntityLayout.Route path="/docs" title="Docs">
